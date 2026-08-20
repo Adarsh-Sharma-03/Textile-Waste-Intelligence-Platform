d01e2cfcd0b512586b92d8c508cbe555a052ab93
Adarsh Sharma <adarshajaysharma@gmail.com>
2026-08-21 01:06:52 +0530

    fix: updated main.py to use  env variable

--- backend/main.py
+++ backend/main.py
@@ -30,7 +30,7 @@ def read_root():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
 
 
 
